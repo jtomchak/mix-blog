f3f9e797a00e6e67818ebb1ad11e7cc707793022
Jesse Tomchak <jtomchak@gmail.com>
2022-11-14 14:29:22 -0700

    fetch articles

--- apps/reblog/app/routes/index.tsx
+++ apps/reblog/app/routes/index.tsx
@@ -1,32 +1,46 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async () => {
+  const { data } = await fetch(
+    "https://04imn06s.api.sanity.io/v1/graphql/production/default",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+  query LastestArticles {
+    allPost(limit: 30, sort:{publishedAt: DESC}) {
+      _id
+      title
+      body
+      slug{
+        current
+      }
+      publishedAt
+      categories{
+        title
+      }
+    }
+}
+  `,
+      }),
+    }
+  ).then((res) => res.json());
+  return data;
+};
+
 export default function Index() {
+  const articleDate = useLoaderData();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Products</h1>
+      {articleDate?.allPost.map((article) => (
+        <div key={article._id}>{article.title}</div>
+      ))}
     </div>
   );
 }
